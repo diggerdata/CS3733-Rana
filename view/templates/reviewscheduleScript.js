42bb6a0db7d567119ad4f1bf5fe7d45f6d1a642a
nickbenoit <nckbnt12@gmail.com>
2018-11-29 15:19:08 -0500

    Working table generation

--- view/templates/reviewscheduleScript.js
+++ view/templates/reviewscheduleScript.js
@@ -6,31 +6,7 @@ function toggleCalendar(arg) {
 	var hideDiv = document.getElementById("hideCal");
 	
 	if (arg){
-		timeSlotsData = showTimeSlots();
-		if (timeSlotsData) {
-			// Get the calendarView <table> element with id="calendar"
-			var calenderTable = document.getElementById("calendar");
-
-			// Keep track of the slots that have been used so far
-			var slot = 0;
-
-			// Calculate the maximum number of rows, from the lenght of time for TimeSlots and the duration of timeslots
-			// var maxRow = (endTime - startTime) / (duration / 60);
-
-			// For each row in the table, fill in the timeslot data
-			for (rowNum = 0; rowNum < maxRow; rowNum++) {
-				// Create a new empty row in the table
-				var row = calenderTable.insertRow(slot);
-
-				// For each of the days in the week (Mon-Fri), add the TimeSlots
-				for(day = 0; day < 5; day++) {
-					var cell = row.insertCell(day);
-					cell.innerHTML = timeSlotsData[slot];
-					slot++;
-				}
-			}
-			
-		}
+		var timeSlotsData = showTimeSlots();
 
 		for (i = 0; i < weekButt.length; i++) {
 			weekButt[i].style.display = 'block';
@@ -56,13 +32,36 @@ function showTimeSlots() {
 
 	// Make GET request
 	request.open('GET', 'https://sqc1z962y5.execute-api.us-east-2.amazonaws.com/dev/schedule/1?week=2011-04-18T00:00:00.00Z', true);
-	request.onload = function getTimeSlots() {
+	request.onload = function () {
 		// Access JSON data
 		var data = JSON.parse(this.response);
 
 		// If the response is ok, put the data in the table
 		if (request.status >= 200 && request.status < 400) {
-			return data;
+			console.log(data.name);
+			
+			// Get the calendarView <table> element with id="calendar"
+			var calenderTable = document.getElementById("calendarBody");
+
+			// Keep track of the slots that have been used so far
+			var slot = 0;
+
+			// Calculate the maximum number of rows, from the lenght of time for TimeSlots and the duration of timeslots
+			// var maxRow = (endTime - startTime) / (duration / 60);
+			var maxRow = 10;
+
+			// For each row in the table, fill in the timeslot data
+			for (rowNum = 0; rowNum < maxRow; rowNum++) {
+				// Create a new empty row in the table
+				var row = calenderTable.insertRow(rowNum);
+
+				// For each of the days in the week (Mon-Fri), add the TimeSlots
+				for(day = 0; day < 5; day++) {
+					var cell = row.insertCell(day);
+					cell.innerHTML = "Test";//timeSlotsData[slot];
+					slot++;
+				}
+			}
 		} else {
 			// Error handling
 		}
